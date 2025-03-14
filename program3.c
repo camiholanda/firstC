46383072a4508e9d8f25fd2f4799deb3e32af404
Camila Holanda <cmdhn.holanda@gmail.com>
2025-03-14 11:37:12 -0300

    Create program3.c

---
+++ program3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* instrução com objetivo de imprimir na tela um dado do tipo char com valor x maiúsculo,
+   sendo x um valor digitado pelo usuário e, em seguida, mostrar x em letra minúscula.
+*/
+
+int main()
+{
+    char x;
+    printf("Digite uma letra maiúscula:");
+    scanf("%c", &x);
+    x = x + 32;
+    printf("Letra minúscula:%c\n",x);
+
+   return(0);
+
+    }
