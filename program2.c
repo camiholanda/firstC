e195850e4e2e6195b4ba9e72a340c3fb6a39f2f3
Camila Holanda <cmdhn.holanda@gmail.com>
2025-03-14 11:11:47 -0300

    Update program2.c

--- program2.c
+++ program2.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 // instrução com objetivo de imprimir na tela um dado do tipo int com valor x, sendo x um valor digitado pelo usuário.
 
 int main()
 {
     int x;
+    printf("Digite um número inteiro:");
     scanf("%d", &x);
 
     printf("Valor de x: %d", x);
     return(0);
 
     }
+
